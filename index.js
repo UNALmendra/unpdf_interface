7892cd7be78dc56dbe4b15b074273383e75d524b
Juan Sierra <jgsierraa@unal.edu.co>
2022-06-05 14:55:00 -0500

    feature: working soap calling node client.js

--- index.js
+++ index.js
@@ -1,20 +1,48 @@
+var soap = require('soap');
 var express = require("express");
+var fs = require('fs');
 
 const axios_ = require("axios");
 const apiUrl = require("./constants").apiUrl;
 const queries = require("./constants").queries;
 
-const getDocumentsUser = async (user) => {
+
+
+
+// --------------------------------------------------------
+// --------------------------------------------------------
+// --------------------------------------------------------
+// --------------------------------------------------------
+// the splitter function, used by the service
+const getDocumentsUser = async (args) => {
   const response = await axios_.post(apiUrl, {
     query: queries.getDocumentsUser,
-    variables: { user: user },
+    variables: { user: args.user },
   });
   return response.data.data;
 };
 
+
+// the service
+var serviceObject = {
+  DocumentsService: {
+        DocumentsServiceSoapPort: {
+            Documents: getDocumentsUser
+        },
+        DocumentsServiceSoap12Port: {
+            Documents: getDocumentsUser
+        }
+    }
+}
+
+var xml = fs.readFileSync('service.wsdl', 'utf8');
+ // --------------------------------------------------------
+ // --------------------------------------------------------
+ // --------------------------------------------------------
+
+
 var app = express();
 app.get("/2E", (req, res) => {
-  getDocumentsUser;
   getDocumentsUser("Johan")
     .then((response) => {
       res.send(response);
@@ -24,5 +52,9 @@ app.get("/2E", (req, res) => {
     });
 });
 
-app.listen(3002);
+app.listen(3002, function () {
+  var wsdl_path = "/wsdl";
+  soap.listen(app, wsdl_path, serviceObject, xml);
+  console.log("Check http://localhost:3002" + wsdl_path +"?wsdl");
+})
 console.log("Running on 3002");
