dbe774b2f736390d08eb3114108c59a31a1fe094
Juan Sierra <jgsierraa@unal.edu.co>
2022-06-05 14:02:44 -0500

    chore: code cleanup

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 var express = require("express");
-var { graphqlHTTP } = require("express-graphql");
-var { buildSchema } = require("graphql");
 
 const axios_ = require("axios");
 const apiUrl = require("./constants").apiUrl;
@@ -14,36 +12,10 @@ const getDocumentsUser = async (user) => {
   return response.data.data;
 };
 
-var schema = buildSchema(`
-type Log {
-    Doc: String
-    Description: String
-    User: String
-    Date: String
-}
-input LogInput{
-  doc: String!
-  user: String!
-  description: String!
-}
-
-type Query {
-    logsById(Doc: String): [Log]
-}
-
-`);
-
-var root = {
-  logsById: async (Doc) => {
-    const response = await getDocumentsUser("10");
-    return response;
-  },
-};
-
 var app = express();
 app.get("/2E", (req, res) => {
-  getDocumentsUser
-getDocumentsUser("Johan")
+  getDocumentsUser;
+  getDocumentsUser("Johan")
     .then((response) => {
       res.send(response);
     })
@@ -52,12 +24,5 @@ getDocumentsUser("Johan")
     });
 });
 
-// app.use(
-//   "/graphql",
-//   graphqlHTTP({
-//     schema: schema,
-//     rootValue: root,
-//     graphiql: true,
-//   })
-// );
 app.listen(3002);
+console.log("Running on 3002");
